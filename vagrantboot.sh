0b6a8e68891c0a1cded5fbee1a7185f0590fa2ff
Jake Bladt <jake@jakebladt.com>
2016-05-13 18:33:32 -0400

    install test tools

--- vagrantboot.sh
+++ vagrantboot.sh
@@ -2,5 +2,8 @@
 
 apt-get update
 
-# install python
-apt-get install python-dev python-pip -q -y
+# install python tools
+apt-get install python3-setuptools
+easy_install3 pip
+pip-3.4 install pylint
+pip-3.4 install pyflakes
