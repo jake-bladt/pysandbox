d4115ae70e7fb2111d352ced562e90ec0925abc8
Jake Bladt <jake@jakebladt.com>
2016-05-13 19:54:55 -0400

    +:

--- 001-unittest/fact.py
+++ 001-unittest/fact.py
@@ -1,7 +1,6 @@
-def factorial(n)
+def factorial(n):
   r = 1
   for x in 1..n:
     r *= (x + 1)
 
   return r
-  
--- 001-unittest/runtests.py
+++ 001-unittest/runtests.py
@@ -1,4 +1,5 @@
 import unittest
+import fact
 
 class TestPython(unittest.TestCase):
   "Testing basic concepts in Python"
@@ -12,6 +13,8 @@ class TestPython(unittest.TestCase):
   def test_basic_addition(self):
   	self.assertEqual(2 + 2, 4)
 
+  def test_factorial(self):
+  	self.assertEqual(fact.factorial(5), 120)
+
 if __name__ == '__main__':
     unittest.main()
-  	
