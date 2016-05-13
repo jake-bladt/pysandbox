5bd392ea5c02442811f0508f5d020b3f84874411
Jake Bladt <jake@jakebladt.com>
2016-05-13 18:58:01 -0400

    --

--- vagrantboot.sh
+++ vagrantboot.sh
@@ -5,5 +5,5 @@ apt-get update
 # install python tools
 apt-get install python3-setuptools
 easy_install3 pip
-pip-3.4 install pylint
-pip-3.4 install pyflakes
+pip3.4 install pylint
+pip3.4 install pyflakes
