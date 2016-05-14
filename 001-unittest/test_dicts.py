d5ca0cf244117e29998980bc792eb743b65ce609
Jake Bladt <jake@jakebladt.com>
2016-05-14 13:31:02 -0400

    string->dict)

--- 001-unittest/test_dicts.py
+++ 001-unittest/test_dicts.py
@@ -7,3 +7,8 @@ class TestDicts(unittest.TestCase):
     ls = [['Yankees', 7], ['Red Sox', 0]]
     d = dict(ls)
     self.assertEqual(d['Yankees'], 7)
+
+  def test_string_to_dict(self):
+    cypher = ['an', 'bo', 'cp', 'dq']
+    mapping = dict(cypher)
+    self.assertEqual(mapping['d'], 'q')
