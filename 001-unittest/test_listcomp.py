2146895d9a307957629edbee69a6e6fb2d12e7db
Jake Bladt <jake@jakebladt.com>
2016-05-15 12:16:22 -0400

    test multivariate

--- 001-unittest/test_listcomp.py
+++ 001-unittest/test_listcomp.py
@@ -7,3 +7,8 @@ class TestListComp(unittest.TestCase):
     all_nums = [x for x in range(1, 1000)]
     for num in all_nums:
       self.assertLess(num, 1000)
+  
+  def test_multivariate_comp(self):
+    sets = [(op1, op2) for op1 in range(1,12) for op2 in range(1, 12)]
+    for ops in sets:
+      self.assertLess(ops[0] * ops[1], 144)
