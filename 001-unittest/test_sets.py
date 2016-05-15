346a617e6485f92a048ba709bfcade0bf8ba9add
Jake Bladt <jake@jakebladt.com>
2016-05-14 22:05:03 -0400

    test in set

---
+++ 001-unittest/test_sets.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestDicts(unittest.TestCase):
+  "Testing set functions"
+
+  def test_dict_keys_as_set(self):
+    stacks = {
+      "Jake": 10785,
+      "Mitch": 5555,
+      "Natalie": 400,
+      "Frannie": 6005 
+    }
+
+    self.assertTrue("Frannie" in set(stacks))
