5bce9e22755fd0270555909af446307247a64287
Jake Bladt <jake@jakebladt.com>
2016-05-14 22:41:20 -0400

    freeform test

---
+++ 001-unittest/test_loops.py
@@ -0,0 +1,10 @@
+import unittest
+
+import lists
+
+class TestLoops(unittest.TestCase):
+  "tests for loops and navigation"
+
+  def test_for(self):
+    for x in lists.get_default_list():
+      self.assertTrue(x > 1)
