0f493f99c58e37f63897cd7be982c9ba2d8bdd66
Jake Bladt <jake@jakebladt.com>
2016-05-15 12:05:38 -0400

    test basic list comprehension

--- 001-unittest/test_listcomp.py
+++ 001-unittest/test_listcomp.py
@@ -4,5 +4,6 @@ class TestListComp(unittest.TestCase):
   "Tests of list comprehension"
 
   def test_basic_comp(self):
-    x for x in range(1, 1000):
-      self.assertLess(x, 1000)
+    all_nums = [x for x in range(1, 1000)]
+    for num in all_nums:
+      self.assertLess(num, 1000)
