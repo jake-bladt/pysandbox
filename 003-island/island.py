2fe40e6108458f2807ca09bdf3fcfdfeb17cd7a4
Jake Bladt <jake@jakebladt.com>
2016-06-04 12:22:15 -0400

    +spec:

--- 003-island/island.py
+++ 003-island/island.py
@@ -1,4 +1,9 @@
 def random_walk(from, to):
+  """
+  This is a random walk along a single 'segment of a coastline' (defined by its start and end points.)
+  Each endpoint is an x, y tuple on a cartesian plane. The random walk should never cross itself and
+  never curve back towards its origin. Each step should be towards the end, even if only a little.
+  """
   pass
 
 def main():
