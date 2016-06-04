14c28868da02a1906a0eaeebb5d990821b255260
Jake Bladt <jake@jakebladt.com>
2016-06-04 12:39:43 -0400

    +min/max delta

--- 003-island/island.py
+++ 003-island/island.py
@@ -1,13 +1,18 @@
 
 
-def random_walk(from, to, min_steps = 19, max_steps = 20, smoothing = 0.5):
+def random_walk(from, to, min_steps = 10, max_steps = 20, smoothing = 0.5):
   """
   This is a random walk along a single 'segment of a coastline' (defined by its start and end points.)
   Each endpoint is an x, y tuple on a cartesian plane. The random walk should never cross itself and
   never curve back towards its origin. Each step should be towards the end, even if only a little.
   """
+  
+  min_delta = (float(from[0]) / max_steps, float(from[1]) / max_steps)
+  max_delta = (float(from[0]) / min_steps, float(from[1]) / min_steps)
+
   current_point = from
   all_points = [from]
+
   while current_point[0] != to[0] or current_point[1] != to[1]:
     pass
   
